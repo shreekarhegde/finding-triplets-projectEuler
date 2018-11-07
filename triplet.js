054b1399d98a4379665781177f6f835f683b1162
shreekar hegde <shreekarhegde9@gmail.com>
2018-11-07 18:26:58 +0530

    checking three numbers if thery are a triplet

---
+++ triplet.js
@@ -0,0 +1,10 @@
+for(var c = 0; c < 1000; c++){
+for(var i = 0; i < 1000; i++){
+    for(var j = 0; j < 1000; j ++){
+        if(((i*i) + (j*j) == c*c && (i+j+c) == 1000)){
+            console.log(i,j,c);
+        }
+    
+}
+}
+}
